23a0a39185569e4e2b0ff9eafe780e0170eaf11a
ajt79 <andrew.thyng@txstate.edui>
2019-09-04 10:03:06 -0500

    Shift to typescript front end

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #e3e8db;
+`
+
+const App: React.FC = () => {
+  return <Container />;
+}
+
+export default App;
